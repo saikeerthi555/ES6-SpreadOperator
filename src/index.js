28b08b85f2d7ee8075f80f73b05b820fdfe0ac20
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-02-11 20:18:32 +0530

    Spread Operator example

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//CHALLENGE: Make the code in App.jsx work.
-//The final app should have a single contact
-//with fName, lName and email.
+const citrus = ["Lime", "Lemon", "Orange", "Mango"];
+const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
+console.log(fruits);
 
-//HINT: You'll need to apply the following things you learnt:
-//1. Using JS Objects with state.
-//2. Making use of previous state when changing state.
-//3. Working with forms in React.
-//4. Handing events
+const fullName = { fName: "James", lName: "Bond" };
+const user = { ...fullName, id: 1, username: "jamesbond007" };
+
+console.log(user);
